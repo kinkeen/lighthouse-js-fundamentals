acdfba7497a7b4ff2b7fa111edf577a78879d17c
Kineret <kineret.braslavsky@gmail.com>
2020-11-15 18:35:36 -0500

    commit new file drivingMayorDaisy.js

---
+++ lighthouse-js-fundamentals/objects- drivingMayorDaisy.js
@@ -0,0 +1,31 @@
+const carPassing = function (cars, speed) {
+  // Your code in here ...
+  let  time = Date.now()
+
+  var person = {
+    time: time,
+    speed: speed
+  }
+  
+  cars.push(person)
+  return cars
+}
+  
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40
+  },
+  {
+    time: 1568329821632,
+    speed: 42
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38;
+
+console.log(carPassing(cars, speed));
